9900b454425765d4734b1de423c0eb00576ac223
Arise Damilare <arisedamilare5@gmail.com>
2026-06-05 21:26:10 +0100

    In app messages, Django cleanup, send mail

--- django_market/settings.py
+++ django_market/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "productApp",
     "userApp",
     'crispy_forms',
-    "crispy_bootstrap5"
+    "crispy_bootstrap5",
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
@@ -144,8 +145,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL=f"Django Market <{EMAIL_HOST_USER}>"
